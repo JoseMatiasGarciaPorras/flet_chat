b019e382a5c7d8fba14ce4fc0949bf7fc694d91c
Jose Matias Garcia <jmgarciaporras@gmail.com>
2025-12-29 19:53:24 +0100

    Funcionalidad basica

--- src/main.py
+++ src/main.py
@@ -2,24 +2,16 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
+    def send_click(e):
+        chat.controls.append(ft.Text(nuevo_mensaje.value))
+        nuevo_mensaje.value = ''
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
-        )
-    )
+    chat = ft.Column()
+    nuevo_mensaje = ft.TextField()
+    boton_enviar = ft.Button('Send', on_click=send_click)
+
+    page.add(chat, ft.Row(controls=[nuevo_mensaje, boton_enviar]))
 
 
 ft.run(main)
